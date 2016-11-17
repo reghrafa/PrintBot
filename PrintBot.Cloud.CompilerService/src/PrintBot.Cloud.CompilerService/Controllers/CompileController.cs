b533a6cdda0a350fc685aa52be9815c5d3b30149
reghrafa <rafael@regh.me>
2016-11-17 19:07:55 +0100

    test

---
+++ PrintBot.Cloud.CompilerService/src/PrintBot.Cloud.CompilerService/Controllers/CompileController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrintBot.Cloud.CompilerService.Controllers
+{
+    [Route("api/[controller]")]
+    public class CompileController : Controller
+    {
+        // POST api/compile
+        [HttpPost]
+        public void Post([FromBody]SourceCodeDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
+
+        }
+    }
+}
