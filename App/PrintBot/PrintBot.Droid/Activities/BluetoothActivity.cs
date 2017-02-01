6917ec6ecb1f5167bd18ec7e06309d0211af24f4
Vav1990 <tland@DESKTOP-KUK6MTR>
2017-02-01 17:24:49 +0100

    icon and bluetooth fixes

--- App/PrintBot/PrintBot.Droid/Activities/BluetoothActivity.cs
+++ App/PrintBot/PrintBot.Droid/Activities/BluetoothActivity.cs
@@ -12,22 +12,45 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using PrintBot.Droid.Fragments;
+using PrintBot.Infrastructure.ViewModels;
 
 namespace PrintBot.Droid.Activities
 {
     [Activity(Label = "Bluetooth Setup")]
     public class BluetoothActivity : PortraitActivity
     {
+        BluetoothViewModel _vm = ServiceLocator.Current.BluetoothViewModel;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.bluetooth_layout);
-            FragmentTransaction ft = FragmentManager.BeginTransaction();
-            ft.Replace(Resource.Id.bluetooth_fragment_container, new BluetoothScanFragment());
-            ft.Commit();
-
+            ChangeFragment();
+            _vm.PropertyChanged += _vm_PropertyChanged;
             ColorDrawable colorDrawable = new ColorDrawable(Color.ParseColor("#3b8686"));
             ActionBar.SetBackgroundDrawable(colorDrawable);
         }
+
+        private void _vm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(BluetoothViewModel.Connected))
+            {
+                ChangeFragment();
+            }
+        }
+
+        private void ChangeFragment()
+        {
+            FragmentTransaction ft = FragmentManager.BeginTransaction();
+            if (!_vm.Connected)
+            {
+                ft.Replace(Resource.Id.bluetooth_fragment_container, new BluetoothScanFragment());
+            }
+            else
+            {
+                ft.Replace(Resource.Id.bluetooth_fragment_container, new BluetoothSendFragment());
+            }
+            ft.Commit();
+        }
     }
+
 }
